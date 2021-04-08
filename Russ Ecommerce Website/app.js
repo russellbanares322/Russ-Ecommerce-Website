05d48aa97c68077275486be0358dbe1af1b071bf
russellbanares322 <81782981+russellbanares322@users.noreply.github.com>
2021-04-09 07:14:50 +0800

    updated website just needed backend.

--- Russ Ecommerce Website/app.js
+++ Russ Ecommerce Website/app.js
@@ -3,9 +3,7 @@ const tl = gsap.timeline({defaults: {ease: "power1.out" } });
 tl.to('.text', { y: "0%", duration: 1, stagger: 0.25 });
 tl.to('.slider', { y: "-100%", duration: 1.5, delay: 0.5 });
 tl.to('.intro', { y: "-100%", duration: 1 }, "-=1");
-tl.fromTo('nav', {opacity: 0}, { opacity: 1, duration: 0});
-tl.fromTo('.btn', {opacity: 0}, { opacity: 1, duration: 0});
-tl.fromTo('.title', {opacity: 0}, { opacity: 1, duration: 0});
+tl.fromTo('.shopping-cart, nav, .logo, .btn, .title, .col-2', {opacity: 0}, { opacity: 1, duration: 0});
 tl.fromTo('.big-text', {opacity: 0}, { opacity: 1, duration: 1});
-tl.fromTo('.col-2', {opacity: 0}, { opacity: 1, duration: 1, stagger: 0.25});
+
 
