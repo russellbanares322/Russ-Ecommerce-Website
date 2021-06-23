d00d8532ed33cea8ce7c7f80ed552802d28a50f7
russellbanares322 <banaresrussell11@gmail.com>
2021-06-23 20:28:48 +0800

    Clean folder unnecessary files

---
+++ Russ Ecommerce Website/css/app.js
@@ -0,0 +1,14 @@
+const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
+
+tl.to(".text", { y: "0%", duration: 1, stagger: 0.25 });
+tl.to(".slider", { y: "-100%", duration: 1.5, delay: 0.5 });
+tl.to(".intro", { y: "-100%", duration: 1 }, "-=1");
+tl.fromTo(
+  ".shopping-cart, nav, .logo, .btn, .title, .col-2",
+  { opacity: 0 },
+  { opacity: 1, duration: 0 }
+);
+tl.fromTo(".big-text", { opacity: 0 }, { opacity: 1, duration: 1 });
+
+
+
